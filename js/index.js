f02126c38e246f41627cf08afaf476f2d5fcb408
shoumik756 <shoumikislam756@gmai.com>
2024-09-23 04:24:59 +0600

    2nd and 3rd card js integrate done and sticky the navbar

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ document.getElementById('donate-now-btn').addEventListener('click', function(){
     const myTotalDonatedBalance = getTotalDonatedbalance ();
 
     if(isNaN(inputFliedValue) ){
-        alert('You Have Input a Invalid Number');
+        alert('You have entered an invalid number');
         return
     }
     if(inputFliedValue > myCurrentBalance ()){
@@ -19,6 +19,55 @@ document.getElementById('donate-now-btn').addEventListener('click', function(){
 
     const currentBalanceValue = myCurrentBalance();
     const currentBalance = currentBalanceValue - inputFliedValue;
-    document.getElementById('current-balance').innerText = currentBalance;
+    document.getElementById('current-balance').innerText = currentBalance.toFixed(2);
 });
+
+// for 2nd card
+document.getElementById('donate-now-btn2').addEventListener('click', function(){
+
+    const inputFliedValue2 = getDonationInputFliedValues2();
+    const myTotalDonatedBalance2 = getTotalDonatedbalance2 ();
+
+    if(isNaN(inputFliedValue2) ){
+        alert('You have entered an invalid number');
+        return
+    }
+    if(inputFliedValue2 > myCurrentBalance ()){
+        alert ('You Have Insufficient Balance For Donation');
+        return
+    }
+
+    const totalDonated2 = inputFliedValue2 + myTotalDonatedBalance2;
+
+    document.getElementById('total-donated-balance2').innerText = totalDonated2;
+    
+    const currentBalanceValue = myCurrentBalance();
+    const currentBalance = currentBalanceValue - inputFliedValue2;
+    document.getElementById('current-balance').innerText = currentBalance.toFixed(2);
+});
+
+// for 3rd card
+document.getElementById('donate-now-btn3').addEventListener('click', function(){
+    
+    const inputFliedValue3 = getDonationInputFliedValues3();
+    const myTotalDonatedBalance3 = getTotalDonatedbalance3 ();
+
+    if(isNaN(inputFliedValue3) ){
+        alert('You have entered an invalid number');
+        return
+    }
+    if(inputFliedValue3 > myCurrentBalance ()){
+        alert ('You Have Insufficient Balance For Donation');
+        return
+    }
+
+    const totalDonated3 = inputFliedValue3 + myTotalDonatedBalance3;
+
+    document.getElementById('total-donated-balance3').innerText = totalDonated3;
+    
+    const currentBalanceValue = myCurrentBalance();
+    const currentBalance = currentBalanceValue - inputFliedValue3;
+    document.getElementById('current-balance').innerText = currentBalance.toFixed(2);
+});
+
 
--- js/utilities.js
+++ js/utilities.js
@@ -16,3 +16,24 @@ function getTotalDonatedbalance (){
     return totalDonatedBalance;
 }
 
+// for 2nd card
+function getDonationInputFliedValues2 (){
+    const donationInputFiled2 = parseFloat(document.getElementById('donation-input-field2').value);
+    return donationInputFiled2
+}
+
+function getTotalDonatedbalance2 (){
+    const totalDonatedBalance2 = parseFloat(document.getElementById('total-donated-balance2').innerText);
+    return totalDonatedBalance2;
+}
+
+// for 3rd card
+function getDonationInputFliedValues3 (){
+    const donationInputFiled3 = parseFloat(document.getElementById('donation-input-field3').value);
+    return donationInputFiled3
+}
+
+function getTotalDonatedbalance3 (){
+    const totalDonatedBalance3 = parseFloat(document.getElementById('total-donated-balance3').innerText);
+    return totalDonatedBalance3;
+}
